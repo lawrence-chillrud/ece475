9ce73ab31d84133f8d6385797c73fe1530fb6bc7
Lawrence Chillrud <lgc2139@columbia.edu>
2022-11-22 04:25:18 -0600

    Saving progress on extract_features.py

--- code/extract_features.py
+++ code/extract_features.py
@@ -3,10 +3,10 @@
 # Date: 11/21/2022
 # Description: From the .nii.gz files in the data/generated folder, this script
 # extracts radiomic features of interest from CT scans & their segmentations
-# using the pyradiomics package
+# using the pyradiomics package. NOTE: Cases R01-059 and R01-119 are weird...
 
 # %%0. Package imports 
-import os
+from glob import glob
 import pandas as pd
 from radiomics import featureextractor
 from datetime import datetime
@@ -19,3 +19,19 @@ set_wd()
 # 2. Read relevant .csv files
 data_dir = 'data/generated/'
 df = pd.read_csv(data_dir + 'NSCLC_labels.csv')
+
+# 3. Extract features from each study in cohort
+extractor = featureextractor.RadiomicsFeatureExtractor()
+weird_studies = ['R01-059', 'R01-119'] # has 10, 3 .nii.gz files respectively
+studies = df['Case ID']
+n = len(studies)
+print(f"Extracting features from {n} studies...")
+print_prog_bar(0, n, prefix='Progress:', suffix='processed study 0/%d, ETA: %s' % (n, 'N/A'), length=25)
+t0 = datetime.now()
+for i, study in enumerate(studies):
+    if study not in weird_studies:
+        files = glob(data_dir + 'NIfTI/' + study + '/*.nii.gz')
+        result = extractor.execute(files[0], files[1])
+        eta = (n - i - 1)*(datetime.now() - t0)/(i + 1)
+    print_prog_bar(i + 1, n, prefix='Progress:', suffix='processed study %d/%d, ETA: %s' % (i + 1, n, str(eta)), length=25)
+# %%
