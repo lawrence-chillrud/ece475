2a611bd9994f8f66e2bec6e7560bbe7178cbb771
Lawrence Chillrud <lgc2139@columbia.edu>
2022-12-05 16:29:41 -0600

    prep_data() function added to utils.py

--- code/utils.py
+++ code/utils.py
@@ -2,8 +2,10 @@
 # Author: Lawrence Chillrud <chili@u.northwestern.edu>
 # Date: 11/21/2022
 # Description: Contains some helper fuctions.
-
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 def print_prog_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
@@ -28,3 +30,25 @@ def str_dict(d):
     for k in d.keys():
         s += '\t' + str(k) + ': ' + str(d[k]) + '\n'
     return s
+
+def prep_data(data_dir='data/generated/', outcome_of_interest='EGFR mutation status', scale='None'):
+    set_wd()
+    dff = pd.read_csv(data_dir + 'NSCLC_features.csv')
+    dfl = pd.read_csv(data_dir + 'NSCLC_labels.csv').filter(['Case ID', 'Age at Histological Diagnosis', 'Weight (lbs)', 'Gender', 'Ethnicity', 'Smoking status', 'Pack Years', outcome_of_interest])
+    dfl = dfl[dfl[outcome_of_interest] != 'Unknown']
+    dfl['Weight (lbs)'] = dfl['Weight (lbs)'].replace('Not Collected', float('nan'))
+    dfl['Pack Years'] = dfl['Pack Years'].replace('Not Collected', float('nan'))
+    dfl['Pack Years'] = dfl['Pack Years'].mask(dfl['Smoking status'] == 'Nonsmoker', 0)
+    dfl['Gender'] = dfl['Gender'].replace(['Male', 'Female'], [0, 1])
+    dfl['Ethnicity'] = dfl['Ethnicity'].replace(['Caucasian', 'Native Hawaiian/Pacific Islander', 'African-American', 'Asian', 'Hispanic/Latino'], [0, 1, 2, 3, 4])
+    dfl[outcome_of_interest] = dfl[outcome_of_interest].replace(['Mutant', 'Wildtype'], [1, -1])
+    dfl['Smoking status'] = dfl['Smoking status'].replace(['Nonsmoker', 'Former', 'Current'], [0, 1, 2])
+    df = pd.merge(dfl, dff, on='Case ID')
+    X = df.drop(['Case ID', outcome_of_interest], axis=1)
+    y = df[outcome_of_interest]
+    if scale == 'Normalize':
+        X = pd.DataFrame(MinMaxScaler().fit_transform(X), columns=X.columns)
+    elif scale == 'Standardize':
+        X = pd.DataFrame(StandardScaler().fit_transform(X), columns=X.columns)
+    
+    return X, y
