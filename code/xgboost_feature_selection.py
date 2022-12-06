81c5d441a2726fca97e2210ce5aa9f10d1474708
Lawrence Chillrud <lgc2139@columbia.edu>
2022-12-06 16:17:20 -0600

    Updated xgboost pipeline

--- code/xgboost_feature_selection.py
+++ code/xgboost_feature_selection.py
@@ -1,3 +1,7 @@
+# File: xgboost_feature_selection.py
+# Author: Lawrence Chillrud <chili@u.northwestern.edu>
+# Date: 11/30/2022
+
 # %%0. Package imports
 import numpy as np
 import pandas as pd
@@ -6,17 +10,15 @@ from sklearn.model_selection import train_test_split, GridSearchCV, RepeatedStra
 from sklearn.metrics import make_scorer, confusion_matrix, f1_score, roc_auc_score, balanced_accuracy_score, matthews_corrcoef, plot_confusion_matrix
 from sklearn.feature_selection import SelectFromModel
 import joblib
-from xgboost import XGBClassifier, plot_importance
+import xgboost as xgb
 import matplotlib.pyplot as plt
 import warnings
 
-# %%1. Set up directories, read in data
+# 1. Set up directories, read in data
 warnings.filterwarnings('ignore')
 X_train_val, X_test, y_train_val, y_test = prep_data(labels=[1, 0])
-#X.hist(grid=False, figsize=[20,10], column=X.columns[1:100])    
-#plt.tight_layout()
 
-# %%2. training and validation splits
+# 2. Training and validation splits
 X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0, stratify=y_train_val)
 
 print("TRAIN: \n", y_train.value_counts())
@@ -26,53 +28,67 @@ print("TEST: \n", y_test.value_counts())
 train_class_counts = y_train.value_counts().values
 inverse_weight = train_class_counts[0]/train_class_counts[1]
 
-# %%3. train xgboost classifier
-estimator = XGBClassifier(
-    objective='binary:logistic',
-    eval_metric='logloss',
-    scale_pos_weight=inverse_weight,
-    use_label_encoder=False
-)
-
-params = {
-    'max_depth': [2, 3],
-    'n_estimators': [100, 150, 200],
-    'learning_rate': [0.1, 0.01],
-    'colsample_bytree': [0.5, 0.75, 1.0],
-    'subsample': [0.6, 0.8, 1.0],
-    'alpha': [0, 1],
-    'min_child_weight': [1, 5]
-}
-
-cv = RepeatedStratifiedKFold(n_splits=4, n_repeats=2, random_state=1)
-
+# 3. Identify metrics we care about (for either gridsearch or cross val later)
 scoring = {'AUC': make_scorer(roc_auc_score), 'F1': make_scorer(f1_score), 'Balanced Acc': make_scorer(balanced_accuracy_score), 'MCC': make_scorer(matthews_corrcoef)}
 
-gs = GridSearchCV(
-    estimator=estimator,
-    param_grid=params,
-    scoring=scoring,
-    refit='AUC',
-    n_jobs=-1,
-    cv=cv,
-    return_train_score=True,
-    verbose=10
-)
+# 4. Set up k-fold cross-val scheme (for either gridsearch or cross val later)
+cv = RepeatedStratifiedKFold(n_splits=4, n_repeats=3, random_state=1)
 
-gs.fit(X_train_val, y_train_val)
-results = gs.cv_results_
-keys = [k for k, v in results.items() if k.startswith('split')]
-for x in keys:
-    del results[x]
+# 5. Decide if gridsearch needed or not
+run_grid_search = False
 
-results_df = pd.DataFrame().from_dict(results)
-print(gs.best_params_)
-joblib.dump(gs.best_estimator_, 'data/generated/models/xgboost.pkl')
-results_df.to_csv('data/generated/models/xgboost_results_df.csv', index=False)
-exit()
+# 6. Set up and run gridsearch of xgboost hyperparameters if need be
+if run_grid_search:
+    # 6a. Initialize XGBoost classifier w/params that we've settled on already
+    estimator = xgb.XGBClassifier(
+        objective='binary:logistic',
+        eval_metric='logloss',
+        scale_pos_weight=inverse_weight,
+        use_label_encoder=False,
+        max_depth=2,
+        alpha=0
+    )
 
-# %%
-model = gs.best_estimator_.fit(X_train, y_train)
+    # 6b. Define grid of hyperparams we want to search through
+    params = {
+        'learning_rate': [0.01, 0.001, 0.05],
+        'n_estimators': [60, 100, 140],
+        'colsample_bytree': [0.75, 0.875, 1],
+        'subsample': [0.5, 0.6, 0.7],
+        'min_child_weight': [3, 5, 10, 20],
+        'lambda': [0, 1],
+        'gamma': [0, 1]
+    }
+
+    # 6c. Initialize gridsearch
+    gs = GridSearchCV(
+        estimator=estimator,
+        param_grid=params,
+        scoring=scoring,
+        refit='AUC',
+        n_jobs=-1,
+        cv=cv,
+        return_train_score=True,
+        verbose=10
+    )
+
+    # 6d. Run gridsearch
+    gs.fit(X_train_val, y_train_val)
+
+    # 6e. Save results
+    results = gs.cv_results_
+    keys = [k for k, v in results.items() if k.startswith('split')]
+    for x in keys:
+        del results[x]
+
+    results_df = pd.DataFrame().from_dict(results)
+    print("\nBEST PARAMS:\n")
+    print(gs.best_params_)
+    joblib.dump(gs.best_estimator_, 'data/generated/models/xgboost_final.pkl')
+    results_df.to_csv('data/generated/models/xgboost_results_df_final.csv', index=False)
+
+# 7. Load best model resulting from gridsearch
+model = joblib.load('data/generated/models/xgboost_final.pkl')
 
 # %%
 y_val_hat = model.predict(X_val)
@@ -89,22 +105,55 @@ print("\nF1 score: ", f1_score(y_train, y_train_hat))
 print("\nMCC: ", matthews_corrcoef(y_train, y_train_hat))
 
 # %%
-plot_importance(model, max_num_features=30)
+y_test_hat = model.predict(X_test)
+print("\nConfusion matrix:\n", confusion_matrix(y_test, y_test_hat))
+print("\nBalanced acc: ", balanced_accuracy_score(y_test, y_test_hat))
+print("\nF1 score: ", f1_score(y_test, y_test_hat))
+print("\nMCC: ", matthews_corrcoef(y_test, y_test_hat))
+
+# %%
+xgb.plot_importance(model, max_num_features=30)
 
 # %%
 thresholds = np.sort(model.feature_importances_)
 thresholds = thresholds[np.where(thresholds != 0)]
-for thresh in thresholds:
-    # select features using threshold
-    selection = SelectFromModel(model, threshold=thresh, prefit=True)
-    select_X_train = selection.transform(X_train)
-    # train model
-    selection_model = XGBClassifier(**model.get_params())
-    selection_model.fit(select_X_train, y_train)
-    # eval model
-    select_X_val = selection.transform(X_val)
-    y_pred = selection_model.predict(select_X_val)
-    f1 = f1_score(y_val, y_pred)
-    print("Thresh=%.3f, n=%d, F1-score: %.3f" % (thresh, select_X_train.shape[1], f1))
-    print("Confusion mat:\n", confusion_matrix(y_val, y_pred))
+thresholds = np.insert(thresholds, 0, 0, axis=0)
+metrics_df = pd.DataFrame()
+X_train_val = X_train_val.reset_index(drop=True)
+y_train_val = y_train_val.reset_index(drop=True)
+for train_index, val_index in cv.split(X_train_val, y_train_val):
+    X_train, X_val = X_train_val.iloc[train_index, :], X_train_val.iloc[val_index, :]
+    y_train, y_val = y_train_val[train_index], y_train_val[val_index]
+    for thresh in thresholds:
+        # select features using threshold
+        selection = SelectFromModel(model, threshold=thresh, prefit=True)
+        select_X_train = selection.transform(X_train)
+
+        # train model
+        selection_model = xgb.XGBClassifier(**model.get_params())
+        selection_model.fit(select_X_train, y_train)
+
+        # eval model
+        select_X_val = selection.transform(X_val)
+        y_pred = selection_model.predict(select_X_val)
+        
+        metrics = pd.DataFrame({
+            'AUC': roc_auc_score(y_val, y_pred), 
+            'F1': f1_score(y_val, y_pred), 
+            'Balanced_Acc': balanced_accuracy_score(y_val, y_pred), 
+            'MCC': matthews_corrcoef(y_val, y_pred), 
+            'thresh': thresh,
+            'val_idx': np.array2string(np.array(val_index))
+        }, index = [0])
+        metrics_df = pd.concat([metrics_df, metrics])
+    print('Done fold')
+    #print("Thresh=%.3f, n=%d, F1-score: %.3f" % (thresh, select_X_train.shape[1], f1))
+    #print("Confusion mat:\n", confusion_matrix(y_val, y_pred))
+
+# %%
+metrics_summary = metrics_df.groupby(['thresh'], as_index=False).mean().sort_values('AUC', ascending=False)
+best_thresh = metrics_summary['thresh'].iloc[0]
+selection = SelectFromModel(model, threshold=best_thresh, prefit=True)
+select_X_train = selection.transform(X_train)
+
 # %%
