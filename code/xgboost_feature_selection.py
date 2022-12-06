460eda6da9ad6d69ebe3bc3a88af9d25415abc9f
Lawrence Chillrud <lgc2139@columbia.edu>
2022-12-06 02:29:50 -0600

    xgboost saving progress...

--- code/xgboost_feature_selection.py
+++ code/xgboost_feature_selection.py
@@ -1,21 +1,23 @@
 # %%0. Package imports
 import numpy as np
+import pandas as pd
 from utils import prep_data
 from sklearn.model_selection import train_test_split, GridSearchCV, RepeatedStratifiedKFold
 from sklearn.metrics import make_scorer, confusion_matrix, f1_score, roc_auc_score, balanced_accuracy_score, matthews_corrcoef, plot_confusion_matrix
 from sklearn.feature_selection import SelectFromModel
+import joblib
 from xgboost import XGBClassifier, plot_importance
 import matplotlib.pyplot as plt
 import warnings
 
 # %%1. Set up directories, read in data
-#warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 X_train_val, X_test, y_train_val, y_test = prep_data(labels=[1, 0])
 #X.hist(grid=False, figsize=[20,10], column=X.columns[1:100])    
 #plt.tight_layout()
 
 # %%2. training and validation splits
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.1, random_state=0, stratify=y_train_val)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0, stratify=y_train_val)
 
 print("TRAIN: \n", y_train.value_counts())
 print("VAL: \n", y_val.value_counts())
@@ -26,31 +28,35 @@ inverse_weight = train_class_counts[0]/train_class_counts[1]
 
 # %%3. train xgboost classifier
 estimator = XGBClassifier(
-    objective='binary:logistic', scale_pos_weight=inverse_weight
+    objective='binary:logistic',
+    eval_metric='logloss',
+    scale_pos_weight=inverse_weight,
+    use_label_encoder=False
 )
 
 params = {
-    'max_depth': range(2, 10, 1),
-    'n_estimators': range(60, 220, 40),
-    'learning_rate': [0.1, 0.01, 0.05],
-    'scale_pos_weight':[1.0, inverse_weight, 2*inverse_weight, 4*inverse_weight, 8*inverse_weight, 16*inverse_weight]
+    'max_depth': [2, 3],
+    'n_estimators': [100, 150, 200],
+    'learning_rate': [0.1, 0.01],
+    'colsample_bytree': [0.5, 0.75, 1.0],
+    'subsample': [0.6, 0.8, 1.0],
+    'alpha': [0, 1],
+    'min_child_weight': [1, 5]
 }
-small_params = {
-    'n_estimators': [60, 100]
-}
-cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+cv = RepeatedStratifiedKFold(n_splits=4, n_repeats=2, random_state=1)
 
 scoring = {'AUC': make_scorer(roc_auc_score), 'F1': make_scorer(f1_score), 'Balanced Acc': make_scorer(balanced_accuracy_score), 'MCC': make_scorer(matthews_corrcoef)}
 
 gs = GridSearchCV(
     estimator=estimator,
-    param_grid=small_params,
+    param_grid=params,
     scoring=scoring,
     refit='AUC',
     n_jobs=-1,
     cv=cv,
-    verbose=True,
-    return_train_score=True
+    return_train_score=True,
+    verbose=10
 )
 
 gs.fit(X_train_val, y_train_val)
@@ -60,13 +66,20 @@ for x in keys:
     del results[x]
 
 results_df = pd.DataFrame().from_dict(results)
-gs.best_params_
+print(gs.best_params_)
+joblib.dump(gs.best_estimator_, 'data/generated/models/xgboost.pkl')
+results_df.to_csv('data/generated/models/xgboost_results_df.csv', index=False)
+exit()
+
 # %%
-y_test_hat = model.predict(X_test)
-print("\nConfusion matrix:\n", confusion_matrix(y_test, y_test_hat))
-print("\nBalanced acc: ", balanced_accuracy_score(y_test, y_test_hat))
-print("\nF1 score: ", f1_score(y_test, y_test_hat))
-print("\nMCC: ", matthews_corrcoef(y_test, y_test_hat))
+model = gs.best_estimator_.fit(X_train, y_train)
+
+# %%
+y_val_hat = model.predict(X_val)
+print("\nConfusion matrix:\n", confusion_matrix(y_val, y_val_hat))
+print("\nBalanced acc: ", balanced_accuracy_score(y_val, y_val_hat))
+print("\nF1 score: ", f1_score(y_val, y_val_hat))
+print("\nMCC: ", matthews_corrcoef(y_val, y_val_hat))
 
 # %%
 y_train_hat = model.predict(X_train)
@@ -80,17 +93,18 @@ plot_importance(model, max_num_features=30)
 
 # %%
 thresholds = np.sort(model.feature_importances_)
-for thresh in thresholds[-30:]:
+thresholds = thresholds[np.where(thresholds != 0)]
+for thresh in thresholds:
     # select features using threshold
     selection = SelectFromModel(model, threshold=thresh, prefit=True)
     select_X_train = selection.transform(X_train)
     # train model
-    selection_model = XGBClassifier(objective='binary:logistic', scale_pos_weight=neg_class_size/pos_class_size, verbosity=0, silent=True)
+    selection_model = XGBClassifier(**model.get_params())
     selection_model.fit(select_X_train, y_train)
     # eval model
-    select_X_test = selection.transform(X_test)
-    y_pred = selection_model.predict(select_X_test)
-    f1 = f1_score(y_test, y_pred)
+    select_X_val = selection.transform(X_val)
+    y_pred = selection_model.predict(select_X_val)
+    f1 = f1_score(y_val, y_pred)
     print("Thresh=%.3f, n=%d, F1-score: %.3f" % (thresh, select_X_train.shape[1], f1))
-    print("Confusion mat:\n", confusion_matrix(y_test, y_pred))
+    print("Confusion mat:\n", confusion_matrix(y_val, y_pred))
 # %%
